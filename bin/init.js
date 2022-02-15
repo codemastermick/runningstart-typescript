5f1c5ac97b478339d91d178879b8935a36e5d986
Mick Evans <51538065+codemastermick@users.noreply.github.com>
2022-02-15 01:23:53 -0500

    chore: clear out init script for now

--- bin/init.js
+++ bin/init.js
@@ -1,21 +1,3 @@
 #! /usr/bin/env node
-const fs = require('fs');
-const { exit } = require('process');
 
-const samplePath = './src/config.sample.json';
-const destinationPath = 'featbuilder.config.json';
-
-if (fs.existsSync(destinationPath)) {
-  console.log('Config file already exists. Exiting...');
-  exit(1);
-}
-
-fs.copyFile(samplePath, destinationPath, (err) => {
-  if (err) {
-    console.log('Error Found:', err);
-    exit(1);
-  } else {
-    console.log('Config file created!');
-    exit(0);
-  }
-});
+console.log('Running npx init...');
